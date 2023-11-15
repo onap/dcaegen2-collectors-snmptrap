76e659582cf6197effa6979c23e21ba5044dac70
Jack Lucas <jflos@sonoris.net>
2023-11-15 12:10:36 -0500

    [SNMPTRAP] Use latest cbs library
    Update to use latest cbs library (2.2.1) in
    requirements.txt.
    Pin pyasn1 library to 0.4.8.  Later versions are
    incompatible with the (no longer maintained) pysnmp
    library.
    Pin python version to 3.8.x in Dockerfile.
    
    Issue-ID: DCAEGEN2-3409
    Change-Id: I34248376d7c4c2fefa97cee51fc4ff341000375b
    Signed-off-by: Jack Lucas <jflos@sonoris.net>

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # ============LICENSE_START=======================================================
 # Copyright (c) 2017-2022 AT&T Intellectual Property. All rights reserved.
 # Copyright (c) 2021 Deutsche Telekom. All rights reserved.
+# Copyright (c) 2023 J. F. Lucas.  All rights reserved.
 # ================================================================================
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,9 +26,9 @@ from setuptools import setup, find_packages
 setup(
     name="snmptrap",
     description="snmp trap receiver for ONAP docker image",
-    version="2.0.7",
+    version="2.0.8",
     packages=find_packages(),
-    install_requires=["pysnmp==4.4.12", "requests==2.18.3", "onap_dcae_cbs_docker_client==2.2.1", "pyyaml"],
+    install_requires=["pysnmp==4.4.12", "pyasn1==0.4.8", "requests==2.18.3", "onap_dcae_cbs_docker_client==2.2.1", "pyyaml"],
     author="Dave L",
     author_email="dl3158@att.com",
     license="Apache 2",
